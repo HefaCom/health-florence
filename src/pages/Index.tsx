188f1229635759b685a78340825588d5bdf02cef
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2025-03-28 15:38:48 +0300

    updates

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -117,7 +117,7 @@ const Index = () => {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <Card className="hidden md:flex p-4 rounded-florence text-center card-glow">
+              <Card className="p-4 rounded-florence text-center card-glow">
                 <div className="mb-3 flex justify-center">
                   <FloLogo className="w-24 h-24" />
                 </div>
