ecbb020f231272cb526576348fc0ad38e88e05c6
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2025-07-10 08:59:03 +0300

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -64,7 +64,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
       let role: UserRole = "user";
       
       // Check for admin role
-      if (attributes === "toptutor0001@gmail.com") {
+      if (attributes === "toptutor0001@gmail.com" ||  email === "trillo3@outlook.com") {
         role = "admin";
       }
       // You can add more role checks here for nurse/doctor in the future
